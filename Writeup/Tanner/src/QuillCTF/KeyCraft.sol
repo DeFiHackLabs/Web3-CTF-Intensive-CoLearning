e869a87497f6873284c149084d817687c4fb60e6
Tanner <ang713943@gmail.com>
2024-08-29 22:57:55 +0800

    Update Tanner 0829

--- Writeup/Tanner/src/QuillCTF/KeyCraft.sol
+++ Writeup/Tanner/src/QuillCTF/KeyCraft.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
--- Writeup/Tanner/src/QuillCTF/WETH10.sol
+++ Writeup/Tanner/src/QuillCTF/WETH10.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.0;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 
 // The Messi Wrapped Ether
