527fb072c387dbcd8491040ef11e72e77a19e3f4
0xbc000 <70671338+0xbc000@users.noreply.github.com>
2024-08-30 11:46:43 +0800

    Update ethernautlv3.sol

--- Writeup/BillyC/ethernaut/test/ethernautlv3.sol
+++ Writeup/BillyC/ethernaut/test/ethernautlv3.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "forge-std/Test.sol";
 import "forge-std/Vm.sol";
 import "openzeppelin-contracts/utils/math/SafeMath.sol";
-import "../../src/levels/03-CoinFlip/CoinFlip.sol"; // test/Billy/ folder
+import "../src/levels/03-CoinFlip/CoinFlip.sol"; 
 
 contract ContractTest4 is Test {
     using SafeMath for uint256;
@@ -22,6 +22,8 @@ contract ContractTest4 is Test {
         );
     }
 
+    // the correct side is predictable as all informations are public
+    // simply take the source code and calculate the side
     function testEthernaut03() public {
         uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
         uint256 blockValue = uint256(blockhash(block.number - 1));
