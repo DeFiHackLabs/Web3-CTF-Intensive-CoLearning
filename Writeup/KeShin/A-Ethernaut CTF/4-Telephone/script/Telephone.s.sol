f81ef3a3743bc743dd0f619f77157b9d29534272
xiaoyuanxun <2060363684@qq.com>
2024-09-01 18:32:38 +0800

    KeShin 9.1 Study Notes

--- Writeup/KeShin/A-Ethernaut CTF/4-Telephone/script/Telephone.s.sol
+++ Writeup/KeShin/A-Ethernaut CTF/4-Telephone/script/Telephone.s.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {TelephoneTest} from "../test/Telephone.t.sol";
 
 contract TelephoneScript is Script {
-    function setUp() public {}
+    function setUp() public {
+        vm.createSelectFork("https://ethereum-sepolia-rpc.publicnode.com");
+    }
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        TelephoneTest telephoneTest = new TelephoneTest();
+
+        telephoneTest.test_ChangeOwner();
     }
 }
