c114ae50f41af61721e4fa0436b816166c452edc
xiaoyuanxun <2060363684@qq.com>
2024-09-10 22:39:57 +0800

    KeShin 9.10 Study Notes

--- Writeup/KeShin/A-Ethernaut CTF/14-GatekeeperTwo/script/GatekeeperTwo.s.sol
+++ Writeup/KeShin/A-Ethernaut CTF/14-GatekeeperTwo/script/GatekeeperTwo.s.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {Attack} from "../test/GatekeeperTwo.t.sol";
 
 contract GatekeeperTwoScript is Script {
-    function setUp() public {}
+    function setUp() public {
+        vm.createSelectFork("https://ethereum-sepolia-rpc.publicnode.com", 6665896);
+    }
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+
+        Attack attack = new Attack();
     }
 }
