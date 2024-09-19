1874214c1606f9eb5bb8b3f841e0ab85d7a7470b
xiaoyuanxun <2060363684@qq.com>
2024-09-19 22:55:00 +0800

    KeShin 9.19 Study Notes

--- Writeup/KeShin/A-Ethernaut CTF/29-Switch/test/Switch.t.sol
+++ Writeup/KeShin/A-Ethernaut CTF/29-Switch/test/Switch.t.sol
@@ -7,10 +7,17 @@ import {Switch} from "../src/Switch.sol";
 contract SwitchTest is Test {
 
     function setUp() public {
-        vm.createSelectFork("https://ethereum-sepolia-rpc.publicnode.com", 6708262);
+        vm.createSelectFork("https://ethereum-sepolia-rpc.publicnode.com", 6721125);
     }
 
     function test_Increment() public {
         Switch switchCA = Switch(0x31cCd4B12dAc76Bba51C707c6d8598D6474B248e);
+
+        // switchCA.flipSwitch(abi.encodeWithSignature("turnSwitchOn()"));
+
+        (bool success,) = address(switchCA).call(abi.encodeWithSignature("turnSwitchOn()"));
+        require(success, "Failed to turn switch on");
+
+        console.log("switchOn: ", switchCA.switchOn());
     }
 }
